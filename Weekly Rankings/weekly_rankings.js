f90fdc8852a54bc7324e2a14498ded95d4cf6bf8
ybgandhi <gandhibyash@gmail.com>
2021-10-11 21:16:38 -0400

    Update weekly_rankings.js

--- Weekly Rankings/weekly_rankings.js
+++ Weekly Rankings/weekly_rankings.js
@@ -1,1 +1,19 @@
 // use this file to manipulate json data
+
+// define SVG area dimension
+var svgWidth = 960;
+var svgHeight = 500;
+
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+var chart = d3.select("scatter").append("div").classed("chart", true);
