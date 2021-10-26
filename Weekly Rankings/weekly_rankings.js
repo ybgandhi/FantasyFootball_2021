7b0c0804f50de45a5ec1f39a339201d35d6b5d81
ybgandhi <gandhibyash@gmail.com>
2021-10-26 17:47:27 -0400

    Update weekly_rankings.js

--- Weekly Rankings/weekly_rankings.js
+++ Weekly Rankings/weekly_rankings.js
@@ -1,43 +1,49 @@
 // use this file to manipulate json data
+fetch("./outputs/test2.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => console.log(data));
 
-d3.json("/outputs/test2.json").then(function(data){
-    console.log(data);
-});
+
+// d3.json("./outputs/test2.json").then(function(data){
+//     console.log(data);
+// });
 
 // chart
-var addChart = function(x_data, y_data, hoverText, metadata) {
-    var metadata_panel = d3.select("chart");
-    metadata_panel.html("");
-    Object.entries(metadata).forEach(([key, value])=>{
-        metadata_panel.append("p").text(`${key}: ${value}`);
-    });
+// var addChart = function(x_data, y_data, hoverText, metadata) {
+//     var metadata_panel = d3.select("chart");
+//     metadata_panel.html("");
+//     Object.entries(metadata).forEach(([key, value])=>{
+//         metadata_panel.append("p").text(`${key}: ${value}`);
+//     });
 
-    var trace = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        type: 'bar',
-        orientation: 'h'
-    };
+//     var trace = {
+//         x: x_data,
+//         y: y_data,
+//         text: hoverText,
+//         type: 'bar',
+//         orientation: 'h'
+//     };
     
-    var data = [trace];
+//     var data = [trace];
 
-    Plotly.newPlot('bar', data);
+//     Plotly.newPlot('bar', data);
 
-    var trace2 = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        mode: 'markers',
-        marker: {
-            size: y_data,
-            color: x_data
-        }
-    };
+//     var trace2 = {
+//         x: x_data,
+//         y: y_data,
+//         text: hoverText,
+//         mode: 'markers',
+//         marker: {
+//             size: y_data,
+//             color: x_data
+//         }
+//     };
 
-    var data2 = [trace2];
+//     var data2 = [trace2];
 
-    Plotly.newPlot('bubble', data2);
+//     Plotly.newPlot('bubble', data2);
 
 
-};
+// };
