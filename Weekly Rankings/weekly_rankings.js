2ecd0042fb092cda13d6f85062de25e4440c62c1
ybgandhi <gandhibyash@gmail.com>
2021-10-26 21:46:22 -0400

    Update weekly_rankings.js

--- Weekly Rankings/weekly_rankings.js
+++ Weekly Rankings/weekly_rankings.js
@@ -1,14 +1,36 @@
 // use this file to manipulate json data
 
-const path = "./outputs/test2.json";
+var url = "https://github.com/ybgandhi/FantasyFootball_2021/blob/31b5e9a9aa4a054559ff95878646e4387b4face1/Weekly%20Rankings/outputs/test2.json";
+d3.json(url).then( function (d){
+    console.log(d);
+})
 
 
-d3.json(path).then(function(data){
-    console.log(data);
-});
 
-// const dataPromise = d3.json(path);
-// console.log("Data Promise: ", dataPromise);
+
+// JSON Data Path
+// const path = "./outputs/test2.json";
+
+// // load table into console log
+// d3.json(path).then(function(data){
+//     console.log(data);
+//     var my_JSON_object = JSON.parse(data.Name);
+//     alert (my_JSON_object[0]);
+// });
+
+
+// d3.open("GET",path, false);
+// d3.send(null)
+// var my_JSON_object = JSON.parse(request.responseText);
+// alert (my_JSON_object[0]);
+
+// d3.json(path).then(function(data){
+
+//         // Grab values from the data json object to build plots
+//         var name = data.dataset.Name;
+//         console.log(data.dataset.Name);
+        //var team = data.dataset.Team;
+
 
 
 // chart
