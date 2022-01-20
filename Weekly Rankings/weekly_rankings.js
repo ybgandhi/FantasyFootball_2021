37584f19b5552faf4a84c4b9115ccf077c3e5bf0
Yash Gandhi <gandhibyash@gmail.com>
2022-01-19 21:26:51 -0500

    Update weekly_rankings.js

--- Weekly Rankings/weekly_rankings.js
+++ Weekly Rankings/weekly_rankings.js
@@ -1,6 +1,32 @@
 // use this file to manipulate json data
 
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
 
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 
 // JSON Data Path
@@ -8,21 +34,28 @@ const path = "./outputs/test2.json";
 
 // load table into console log
 d3.json(path).then(function(week){
-     console.log(week);
+
+     // console log json    
+     console.log(JSON.stringify(week));
      console.log(Object.keys(week));
      console.log(Object.values(week));
      console.log(Object.entries(week));
-     console.log(Object.values(week[1].Name));
-     var Weeks = {}
+     
 
-     for (var i=0;i<week.length;i++) {
-        Weeks[d[i].Team=d[i].Team]
-        console.log(Weeks[d[i].Team])
-     };
+
+
+     week.forEach(function(data) {
+        console.log(data)
+     });
+});
+
+    // for (var i=0;i<week.length;i++) {
+      //  Weeks[d[i].Team=d[i].Team]
+     //   console.log(Weeks[d[i].Team])
+    // };
     // console.log(playerNames);
 //     var my_JSON_object = JSON.parse(data.Name);
 //     alert (my_JSON_object[0]);
-});
 // d3.open("GET",path, false);
 // d3.send(null)
 // var my_JSON_object = JSON.parse(request.responseText);
